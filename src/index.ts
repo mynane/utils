1dd7e8b7117ecf8dab0cd25f609dd3d7af570009
”石进华“ <shijinhua@xiaoduotech.com>
2021-03-04 00:12:03 +0800

    eslint init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-function Test() {}
+interface person {
+  name: string;
+  age: number;
+}
 
-export default Test;
+let tom: person = {
+  name: "Tom",
+  age: 25,
+};
+
+if (tom.age == 25) {
+  console.log(tom.name + "is 25 years old.");
+}
