aef5c558dfff74e8ecc17fbd1263e6ac04d6803a
”石进华“ <shijinhua@xiaoduotech.com>
2021-03-08 11:54:40 +0800

    add type judgment

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,60 @@
+import { TBasicType } from "../types";
+
+const _ToStringCall = (target: TBasicType) =>
+  Object.prototype.toString.call(target);
+
 /**
  * 判断是否是字符串
  * @param target
  */
-export const isString = (target: any) => typeof target === "string";
+export const isString = (target: any) =>
+  _ToStringCall(target) === "[object String]";
+
+/**
+ * 判断是否是数字
+ * @param target
+ */
+export const isNumber = (target: any) =>
+  _ToStringCall(target) === "[object Number]";
+
+/**
+ * 判断是否是boolean
+ * @param target
+ */
+export const isBoolean = (target: any) =>
+  _ToStringCall(target) === "[object Boolean]";
+
+/**
+ * 判断是否是null
+ * @param target
+ */
+export const isNull = (target: any) =>
+  _ToStringCall(target) === "[object Null]";
+
+/**
+ * 判断是否是undefined
+ * @param target
+ */
+export const isUndefined = (target: any) =>
+  _ToStringCall(target) === "[object Undefined]";
+
+/**
+ * 判断是否是object
+ * @param target
+ */
+export const isObject = (target: any) =>
+  _ToStringCall(target) === "[object Object]";
+
+/**
+ * 判断是否是array
+ * @param target
+ */
+export const isArray = (target: any) =>
+  _ToStringCall(target) === "[object Array]";
+
+/**
+ * 判断是否是symbol
+ * @param target
+ */
+export const isSymbol = (target: any) =>
+  _ToStringCall(target) === "[object Symbol]";
--- tests/shared/types.test.ts
+++ tests/shared/types.test.ts
@@ -1,4 +1,14 @@
-import { firstLetterToUpperCase, isString } from "../../src";
+import {
+  firstLetterToUpperCase,
+  isArray,
+  isBoolean,
+  isNull,
+  isNumber,
+  isObject,
+  isString,
+  isSymbol,
+  isUndefined,
+} from "../../src";
 
 import * as chai from "chai";
 
@@ -10,5 +20,80 @@ describe("isString function test", () => {
     expect(isString(undefined)).to.equal(false);
     expect(isString(null)).to.equal(false);
     expect(isString(false)).to.equal(false);
+  });
+});
+
+describe("isNumber function test", () => {
+  it("this function result should correct", () => {
+    expect(isNumber("abcd")).to.equal(false);
+    expect(isNumber(1)).to.equal(true);
+    expect(isNumber(undefined)).to.equal(false);
+    expect(isNumber(null)).to.equal(false);
+    expect(isNumber(false)).to.equal(false);
+  });
+});
+
+describe("isNull function test", () => {
+  it("this function result should correct", () => {
+    expect(isNull("abcd")).to.equal(false);
+    expect(isNull(1)).to.equal(false);
+    expect(isNull(undefined)).to.equal(false);
+    expect(isNull(null)).to.equal(true);
+    expect(isNull(false)).to.equal(false);
+  });
+});
+
+describe("isUndefined function test", () => {
+  it("this function result should correct", () => {
+    expect(isUndefined("abcd")).to.equal(false);
+    expect(isUndefined(1)).to.equal(false);
+    expect(isUndefined(undefined)).to.equal(true);
+    expect(isUndefined(null)).to.equal(false);
+    expect(isUndefined(false)).to.equal(false);
+  });
+});
+
+describe("isBoolean function test", () => {
+  it("this function result should correct", () => {
+    expect(isBoolean("abcd")).to.equal(false);
+    expect(isBoolean(1)).to.equal(false);
+    expect(isBoolean(undefined)).to.equal(false);
+    expect(isBoolean(null)).to.equal(false);
+    expect(isBoolean(false)).to.equal(true);
+  });
+});
+
+describe("isObject function test", () => {
+  it("this function result should correct", () => {
+    expect(isObject("abcd")).to.equal(false);
+    expect(isObject(1)).to.equal(false);
+    expect(isObject(undefined)).to.equal(false);
+    expect(isObject(null)).to.equal(false);
+    expect(isObject(false)).to.equal(false);
+    expect(isObject({})).to.equal(true);
+    expect(isObject(new Object())).to.equal(true);
+  });
+});
+
+describe("isArray function test", () => {
+  it("this function result should correct", () => {
+    expect(isArray("abcd")).to.equal(false);
+    expect(isArray(1)).to.equal(false);
+    expect(isArray(undefined)).to.equal(false);
+    expect(isArray(null)).to.equal(false);
+    expect(isArray(false)).to.equal(false);
+    expect(isArray([])).to.equal(true);
+    expect(isArray(new Array())).to.equal(true);
+  });
+});
+
+describe("isSymbol function test", () => {
+  it("this function result should correct", () => {
+    expect(isSymbol("abcd")).to.equal(false);
+    expect(isSymbol(1)).to.equal(false);
+    expect(isSymbol(undefined)).to.equal(false);
+    expect(isSymbol(null)).to.equal(false);
+    expect(isSymbol(false)).to.equal(false);
+    expect(isSymbol(Symbol())).to.equal(true);
   });
 });
